c26510ed100699acce7fe90c2430d3aab510e92a
luis231298 <pepeluna2385@gmail.com>
2021-01-14 14:56:06 -0600

    cambio posicion camera

--- Proyecto_Teoria/Proyecto_Teoria/proy.cpp
+++ Proyecto_Teoria/Proyecto_Teoria/proy.cpp
@@ -39,7 +39,7 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 35.0f, 10.0f));
 float MovementSpeed = 1.0f;
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
